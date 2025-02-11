b1c1fbaea668987a6beb6c3f8ced1401f37cd8cd
januardineky <januardinekyputra@gmail.com>
2025-02-11 13:17:36 +0700

    1st Commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,7 +53,7 @@
         <!-- Login Form End -->
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="js/main.js"></script>
+    {{--  <script src="js/main.js"></script>  --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.24/dist/sweetalert2.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script>
